f7a9bbc2e5081701b9cd6537d89d77a18332c589
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-01-25 21:27:16 +0530

    css update

--- src/components/Home.js
+++ src/components/Home.js
@@ -11,6 +11,7 @@ export default function Home(props) {
           <Link to="/upper">UpperCase</Link>
           <Link to="/lower">LowerCase</Link>
           <Link to="/convertxml">HTML to XML</Link>
+          <Link to="/convertxml">XML to HTML</Link>
           <Link to="/about">About</Link>
           <Link to="/contact">Constact Us</Link>
         </div>
