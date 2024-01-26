cffeee2394a71bcca9544579ee29b10cb819fcc0
Rithesh Jaston D'sa <ritheshjaston22@gmail.com>
2024-01-26 10:45:05 +0530

    css update

--- src/components/Hometab.js
+++ src/components/Hometab.js
@@ -4,6 +4,9 @@ export default function Hometab() {
   return (
     <div className='homer'>
         <img className='homeimager' src={homeimg} alt="Image"/>
+        <h2 id='hometext'>
+          Texter | Code With R
+        </h2>
     </div>
   )
 }
